04843484f3a902b2e421db3156520bce7a965b52
mangxiao <mangxiao2018@126.com>
2021-09-27 17:35:27 +0800

    工程结构搭建

---
+++ code/probe-sample/bytebuddy-sample/src/main/java/com/mangxiao/probe/bytebuddy/PreMain.java
@@ -0,0 +1,14 @@
+package com.mangxiao.probe.bytebuddy;
+
+import java.lang.instrument.Instrumentation;
+
+public class PreMain {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+
+    }
+
+    public static void premain(String agentArgs) {
+
+    }
+}
