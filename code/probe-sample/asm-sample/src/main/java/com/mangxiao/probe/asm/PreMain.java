3ee597777dfe037b44e5f1bf89e31f3faf94a860
mangxiao <mangxiao2018@126.com>
2021-09-30 14:46:01 +0800

    asm探针

--- code/probe-sample/asm-sample/src/main/java/com/mangxiao/probe/asm/PreMain.java
+++ code/probe-sample/asm-sample/src/main/java/com/mangxiao/probe/asm/PreMain.java
@@ -1,11 +1,13 @@
 package com.mangxiao.probe.asm;
 
+import com.mangxiao.probe.asm.core.ProfilingTransformer;
+
 import java.lang.instrument.Instrumentation;
 
 public class PreMain {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-
+        inst.addTransformer(new ProfilingTransformer());
     }
 
     public static void premain(String agentArgs) {
---
+++ code/probe-sample/asm-sample/src/main/java/com/mangxiao/probe/asm/core/ProfilingTransformer.java
@@ -0,0 +1,35 @@
+package com.mangxiao.probe.asm.core;
+
+import com.mangxiao.probe.asm.filter.ProfilingFilter;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+public class ProfilingTransformer implements ClassFileTransformer {
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        try {
+            if (ProfilingFilter.isNotNeedInject(className)){
+                return classfileBuffer;
+            }
+            return getBytes(loader, className, classfileBuffer);
+        }catch (Exception e){
+
+        }
+        return classfileBuffer;
+    }
+
+
+    private byte[] getBytes(ClassLoader loader, String className, byte[] classfileBuffer) {
+        ClassReader cr = new ClassReader(classfileBuffer);
+        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_MAXS);
+        ClassVisitor cv = new ProfilingClassAdapter(cw, className);
+        cr.accept(cv, ClassReader.EXPAND_FRAMES);
+        return cw.toByteArray();
+    }
+}
